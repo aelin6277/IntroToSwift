94ed235429f984665ac0e7c67e96a51a6aef52e4
elina <aelin6277@gmail.com>
2023-01-07 01:47:51 +0100

    Initial Commit

---
+++ IntroToSwift/IntroToSwiftApp.swift
@@ -0,0 +1,17 @@
+//
+//  IntroToSwiftApp.swift
+//  IntroToSwift
+//
+//  Created by Elin A on 2023-01-07.
+//
+
+import SwiftUI
+
+@main
+struct IntroToSwiftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
